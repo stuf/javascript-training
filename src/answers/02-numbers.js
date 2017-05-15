9d88938939b98286810368fb97c2f1a3deb50292
Stefan Rimaila <stefan@rimaila.fi>
2017-05-15 23:31:42 +0300

    Add second task with model answers

---
+++ src/answers/02-numbers.js
@@ -0,0 +1,39 @@
+// Model answers for `02-numbers`
+
+function addTwo(a, b) {
+  return a + b;
+}
+
+//
+
+function getSquare(a) {
+  return a * a;
+}
+
+//
+
+function getArea(a, b) {
+  return a * b;
+}
+
+//
+
+function getCircumference(radius) {
+  return 2 * radius * Math.PI;
+}
+
+//
+
+function getAverage(a, b) {
+  return (a + b) / 2;
+}
+
+//
+
+module.exports = {
+  addTwo,
+  getSquare,
+  getArea,
+  getCircumference,
+  getAverage
+};
---
+++ src/tasks/02-numbers.js
@@ -0,0 +1,82 @@
+/**
+ * Add `a` and `b` and return their sum
+ *
+ * @example
+ *   input: (2, 4)
+ *   output: 6
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function addTwo(a, b) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Calculate the square for the given number `a`
+ *
+ * @example
+ *   input: (4)
+ *   output: 16
+ *
+ * @param {number} a
+ * @returns {number}
+ */
+function getSquare(a) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Calculate the area for a rectangle the size of `a` * `b`
+ *
+ * @example
+ *   input: (5, 4)
+ *   output: 20
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function getArea(a, b) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Calculate the circumference for a circle with `radius`
+ *
+ * @example
+ *   input: (5)
+ *   output: 31.4159...
+ *
+ * @param {number} radius
+ * @returns {number}
+ */
+function getCircumference(radius) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Calculate the average of `a` and `b`
+ *
+ * @example
+ *   input: (5, 3)
+ *   output: 4
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function getAverage(a, b) {
+  throw new Error('Not implemented');
+}
+
+//
+
+module.exports = {
+  addTwo,
+  getSquare,
+  getArea,
+  getCircumference,
+  getAverage
+};
